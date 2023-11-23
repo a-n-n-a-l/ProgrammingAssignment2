260f81bbf75557371c038b513b0a8e35b6cc5802
a-n-n-a-l <malyshka@hotmail.com>
2023-11-23 11:01:26 -0500

    Created the functions as requested in the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,63 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+
+
+# This function creates a global environment variable for the inverse for the matrix
+# and sets and gets the matrix and its inverse
+
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL            #initialize an empty variable
+  
+  set <-
+    function(y) {
+      #set the value of the matrix in a global variable
+      x <<- y
+      inv <<- NULL
+    }
+  
+  get <- function()
+    x                  #return the current matrix
+  
+  setInverse <-
+    function(invMatrix)
+      inv <<- invMatrix     #get the inverse matrix value
+  
+  getInverse <-
+    function()
+      inv                   #return the inverse matrix value
+  
+  list(
+    #create a list with named values from the function, to make it easier to access
+    set = set,
+    get = get,
+    setInverse = setInverse,
+    getInverse = getInverse
+  )
 }
 
 
-## Write a short comment describing this function
+# This function checks to see if there's a cached version of the inverse of the matrix and return it
+# or calculates an inverse if one doesn't yet exist in the cache
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <-
+  function(x, ...) {
+    # Return the inverse of the matrix 'x'
+    inv <-
+      x$getInverse()                      #retrieve the inverse matrix from the passed object
+
+    if (!is.null(inv)) {
+      #check if there's a matrix already cached
+      message("Getting cached data:")          #alert the user that the matrix that will be returned has already been cached
+      return(inv)           #return cached data
+    }
+
+    data <- x$get()                      
+    inv <- solve(data, ...)              #calculate the inverse matrix
+    x$setInverse(inv) 
+    
+    inv                                   #return the calculated inverse
+    
+  }
